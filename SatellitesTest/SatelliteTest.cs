772b2aac6591e9659f98032bb5b7b293370b2bd5
Carlos Mijangos <manchester-714@hotmail.com>
2021-11-19 11:27:00 -0600

    testing

--- SatellitesTest/SatelliteTest.cs
+++ SatellitesTest/SatelliteTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Quasar.Domain.Aggregation;
+using Quasar.Domain.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,7 +19,14 @@ namespace SatellitesTest
         [TestMethod]
         public void GetSatelliteLocation() 
         {
-            var mockServicio = new SaltellitesService();
+            var mock = new Mock<ISetallitesRepository>();
+
+            SaltellitesService servicio = new SaltellitesService(mock.Object);
+            var request = new RequestSatelliteByDistance() { Distance = 100 };
+            var response = new ResponseSatellites() { Name = "Kenobi" };
+            var satellite = servicio.GetLocation(request);
+
+            Assert.AreEqual(satellite.Result.Name, response.Name);
         }
        
 
